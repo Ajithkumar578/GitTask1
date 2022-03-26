e64dda40b9cff9bf7789fc397317134eaaa25fd7
Ajithkumar578 <ajithkumarece15@gmail.com>
2022-03-26 07:11:01 -0700

    git task

---
+++ src/com/ii/JavaClass.java
@@ -0,0 +1,16 @@
+package com.ii;
+
+public class JavaClass {
+	
+	public void a() {
+		
+		System.out.println("hi");
+	}
+	
+	public static void main(String[] args) {
+		JavaClass h=new JavaClass();
+		h.a();
+		
+	}
+
+}
