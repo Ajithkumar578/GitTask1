4c37cb5007e186e21bba07c0a8c369d26014f08f
Ajithkumar578 <54540135+Ajithkumar578@users.noreply.github.com>
2022-03-28 10:55:21 -0700

    Adding b method

--- src/com/ii/JavaClass.java
+++ src/com/ii/JavaClass.java
@@ -6,11 +6,17 @@ public class JavaClass {
 		
 		System.out.println("hi");
 	}
+	public void b() {
+		
+		System.out.println("hi");
+	}
 	
 	public static void main(String[] args) {
 		JavaClass h=new JavaClass();
 		h.a();
 		
 	}
+	
+	
 
 }
